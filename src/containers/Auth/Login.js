f91e6083174ec33f3643bdaf8067f051d297b1bb
Vladimir Riz <resin.fhp@gmail.com>
2021-04-21 21:16:07 +0300

    feat: user login with token

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { required, length, email } from '../../util/validators';
 
 import { Form, Button, Container, Row } from 'react-bootstrap';
+import Spinner from '../../UI/Spinner';
 
 class Login extends Component {
   state = {
@@ -19,26 +20,33 @@ class Login extends Component {
         touched: false,
         validators: [required, length({ min: 5 })],
       },
-      formIsValid: false,
     },
+    formIsValid: false,
+    loading: false,
+    isAuth: false,
+    userId: null,
+    token: null,
+    error: null,
   };
 
-  inputChangeHandler = (input, value) => {
+  inputChangeHandler = ({ target }) => {
     this.setState((prevState) => {
       let isValid = true;
-      for (const validator of prevState.loginForm[input].validators) {
-        isValid = isValid && validator(value);
+      for (const validator of prevState.loginForm[target.name].validators) {
+        isValid = isValid && validator(target.value);
       }
+
       const updatedForm = {
         ...prevState.loginForm,
-        [input]: {
-          ...prevState.loginForm[input],
+        [target.name]: {
+          ...prevState.loginForm[target.name],
           valid: isValid,
-          value: value,
+          value: target.value,
         },
       };
       let formIsValid = true;
       for (const inputName in updatedForm) {
+        console.log(updatedForm[inputName].valid);
         formIsValid = formIsValid && updatedForm[inputName].valid;
       }
       return {
@@ -62,30 +70,111 @@ class Login extends Component {
     });
   };
 
+  setAutoLogout = (milliseconds) => {
+    setTimeout(() => {
+      this.logoutHandler();
+    }, milliseconds);
+  };
+
+  onLogin = (e) => {
+    e.preventDefault();
+    this.setState({
+      loading: true,
+    });
+    fetch('http://localhost:8080/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: this.state.loginForm.email.value,
+        password: this.state.loginForm.password.value,
+      }),
+    })
+      .then((res) => {
+        if (res.status === 422) {
+          throw new Error('Validation failed.');
+        }
+        if (res.status !== 200 && res.status !== 201) {
+          console.log('Error!');
+          throw new Error('Could not authenticate you!');
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        console.log(resData);
+        this.setState({
+          isAuth: true,
+          token: resData.token,
+          loading: false,
+          userId: resData.userId,
+        });
+        localStorage.setItem('token', resData.token);
+        localStorage.setItem('userId', resData.userId);
+        const remainingMilliseconds = 60 * 60 * 1000;
+        const expiryDate = new Date(
+          new Date().getTime() + remainingMilliseconds
+        );
+        localStorage.setItem('expiryDate', expiryDate.toISOString());
+        this.setAutoLogout(remainingMilliseconds);
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({
+          isAuth: false,
+          loading: false,
+          error: err,
+        });
+      });
+  };
+
+  showForm = () => {
+    const { email, password } = this.state.loginForm;
+    return this.state.loading ? (
+      <Spinner />
+    ) : (
+      <Form onSubmit={this.onLogin}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            onChange={this.inputChangeHandler}
+            value={email.value}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={this.inputChangeHandler}
+            value={password.value}
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button
+          variant="primary"
+          type="submit"
+          disabled={!this.state.formIsValid}
+        >
+          Submit
+        </Button>
+      </Form>
+    );
+  };
+
   render() {
     return (
       <Container className="mt-5">
-        <Row className="m-5 justify-content-center">
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Row>
+        <Row className="m-5 justify-content-center">{this.showForm()}</Row>
       </Container>
     );
   }
